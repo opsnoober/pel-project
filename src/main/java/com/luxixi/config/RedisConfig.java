0cc9a09ccfd917a55bd93c6312549ad418c56fb6
luxixi55 <1660443645@qq.com>
2020-07-31 00:17:36 +0800

    提交

--- src/main/java/com/luxixi/config/RedisConfig.java
+++ src/main/java/com/luxixi/config/RedisConfig.java
@@ -1,9 +1,7 @@
 package com.luxixi.config;
 
-import com.luxixi.PerProjectApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
